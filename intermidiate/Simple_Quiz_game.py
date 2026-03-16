045d82d17dfcc2fd191dd34891067ad82c016c2b
HackWithKing <sumitbharadia4@gmail.com>
2026-03-16 06:15:03 -0700

    Add Simple Quiz Game with scoring system

---
+++ intermidiate/Simple_Quiz_game.py
@@ -0,0 +1,13 @@
+score = 0
+
+answer = input("What is the capital of India? ")
+
+if answer.lower() == "delhi":
+    score += 1
+
+answer = input("2 + 2 = ? ")
+
+if answer == "4":
+    score += 1
+
+print("Your score:", score)
